70eab5761a0987d8c70682293e58554c21c4e18a
deasydoesit <darwin.rinderer@gmail.com>
2018-12-12 21:32:08 -0500

    Creating basic file structure

---
+++ src/components/StoryboardContainer/StoryboardContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './StoryboardContainer.css';
+
+class StoryboardContainer extends Component {
+
+  render() {
+    return (
+      <div>Hi</div> 
+    );
+  }
+}
+
+export default StoryboardContainer;
