c05047bdf0b00af4c9ef076aa94259cfbd937e94
deasydoesit <darwin.rinderer@gmail.com>
2018-12-19 20:10:46 -0500

    Adding country flags to bubble items

--- src/components/BubbleGraph/BubbleGraph.js
+++ src/components/BubbleGraph/BubbleGraph.js
@@ -8,6 +8,7 @@ import HorizontalSlider from './HorizontalSlider';
 import landData from '../../data/BubbleGraph/land-area-by-country.csv';
 import populationData from '../../data/BubbleGraph/population-by-country-2000-2014.csv';
 import co2Data from '../../data/BubbleGraph/co2-by-country-2000-2014.csv';
+import countryFlags from '../../data/BubbleGraph/country-flags.csv'
 
 import './BubbleGraph.css';
 
@@ -18,6 +19,7 @@ const colorRanges = {
 
 class BubbleGraph extends Component {
   state = {
+    countryFlags: null,
     data: null,
     totalPopData: null,
     totalCo2Data: null,
@@ -30,13 +32,15 @@ class BubbleGraph extends Component {
       d3.csv(landData),
       d3.csv(populationData),
       d3.csv(co2Data),
+      d3.csv(countryFlags),
     ])
     .then((files) => {
-      console.log(files);
+      console.log(files)
       this.setState({
         totalLandData: files[0],
         totalPopData: files[1],
         totalCo2Data: files[2],
+        countryFlags: files[3],
       }, () => {
         this.setState({
           data: this.setData(2000),
@@ -46,53 +50,51 @@ class BubbleGraph extends Component {
   }
 
   componentDidUpdate() {
-    //   setTimeout(() => {
-    //     this.setState({
-    //         data: this.setData(2014),
-    //       })
-    //   }, 1000)
 
+    let diameter = [];
 
-    // d3.selectAll('circle').each(function() {
-    //     const el = this;
-    //     d3.select(el.parentNode)
-    //       .insert("svg")
-    //       .attr("class", "wrapped")
-    //       .append(function() { return el; });
-    // })
-    //     .attr('fill', 'url(#locked2)')
+    d3.selectAll('circle').each(function(d){
+      diameter.push(Math.ceil(d3.select(this).attr('r') * 2 ));
+    })
 
-    // d3.selectAll('.wrapped')
-    //   .append("defs")
-    //   .append('pattern')
-    //     .attr('class', 'locked2')
-    //     .attr('patternUnits', 'userSpaceOnUse')
-    //     .attr('width', 400)
-    //     .attr('height', 400)
-    //   .append("image")
-    //     .attr("x", "0")
-    //     .attr("y", "0")
-    //     .attr("xlink:href", "https://cdn.shopify.com/s/files/1/1332/9131/products/Round_Beach_Towel_-_Patriot_-_Mimosa_24f7f284-086b-413a-8140-456915d64740.png?v=1490132816")
-    //     .attr('width', 400)
-    //     .attr('height', 400);
+    console.log(diameter)
+
+    d3.selectAll('circle').each(function() {
+        const el = this;
+        d3.select(el.parentNode)
+          .insert("svg")
+          .attr("class", "wrapped")
+          .append(function() { return el; });
+    })
+        .attr('style', (d, i) => `fill: url(#country-${i}) !important` )
+
+    d3.selectAll('.wrapped')
+      .append("defs")
+      .append('pattern')
+        .attr('id', (d, i) => `country-${i}`)
+        .attr('width', '100%')
+        .attr('height', '100%')
+      .append("image")
+        .attr("xlink:href", (d, i) => this.state.countryFlags[i]['URL'])
+        .attr('width', (d, i) => diameter[i])
+        .attr('height', (d, i) => diameter[i])
+
   }
 
   setData = year => {
     const { totalCo2Data, totalLandData, totalPopData } = this.state;
     return new Array(32).fill(0).map((row, i) => ({
-      x: parseInt(totalLandData[i].Area),
-      y: parseInt(totalPopData[i][year]),
-      size: parseInt(totalCo2Data[i][year]),
-      color: 10,
-      opacity: 0.5
+      Country: totalPopData[i]['Country'],
+      x: parseInt(totalCo2Data[i][year]),
+      y: parseInt(totalLandData[i].Area),  
+      size: parseInt(totalPopData[i][year]), 
     }));
   }
 
   updateBubbleGraph = year => {
-    console.log(year);
     this.setState({
       data: this.setData(year),
-    })
+    });
   }
 
   render() {
@@ -118,22 +120,22 @@ class BubbleGraph extends Component {
             height={600}
             yType='log'
             xType='log'
+             xDomain={[15000, 30000000]}
+             yDomain={[15000, 30000000]}
             noHorizontalGridLines
             noVerticalGridLines
-            >
+          >
             <XAxis 
                 position="end"
-                title="Land area (sq. km)"
+                title=""
                 tickLabelAngle={-45}
-                tickTotal={10}
                 tickFormat={ (value, i, scale, tickTotal) => {
-                return `${scale.tickFormat(tickTotal, '.0s')(value)}`
+                  return `${scale.tickFormat(10, '.0s')(value)}`
                 }}
             />
             <YAxis 
                 position="end"
-                title="Population"
-                tickTotal={7}
+                title=""
                 tickFormat={ (value, i, scale, tickTotal ) => {
                     return `${scale.tickFormat(10, '.0s')(value)}`
                 }}
@@ -162,4 +164,6 @@ export default BubbleGraph;
 // To Do:
 // 1) Sizes of bubbles
 // 2) Background images
+    // make array of images, alphabetized
+    // pass image in based on index
 // 3) Change via slider
