907d0b116cb7b1dcaf8d77d1d226a6d9ff2c7ff7
deasydoesit <darwin.rinderer@gmail.com>
2018-12-22 19:44:40 -0500

    Updates

---
+++ precache-manifest.633ce79f51a2ca6584a7511447436eec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "88f1cef73ee73fa82ff91f659201c38d",
+    "url": "/climate-change-storyboard/static/media/population-by-country-2000-2014.88f1cef7.csv"
+  },
+  {
+    "revision": "b68bd6734b424da05971",
+    "url": "/climate-change-storyboard/static/css/main.5a53b507.chunk.css"
+  },
+  {
+    "revision": "294f5d2625ba41d78612",
+    "url": "/climate-change-storyboard/static/js/1.294f5d26.chunk.js"
+  },
+  {
+    "revision": "7d1d36261f3396e2b910",
+    "url": "/climate-change-storyboard/static/js/runtime~main.7d1d3626.js"
+  },
+  {
+    "revision": "3064315f80e2eb91671c0f68c05a3526",
+    "url": "/climate-change-storyboard/static/media/land-area-by-country.3064315f.csv"
+  },
+  {
+    "revision": "b68bd6734b424da05971",
+    "url": "/climate-change-storyboard/static/js/main.b68bd673.chunk.js"
+  },
+  {
+    "revision": "5cb5bbd61b1c91859cf1e28eb3d9d764",
+    "url": "/climate-change-storyboard/static/media/co2-by-country-2000-2014.5cb5bbd6.csv"
+  },
+  {
+    "revision": "a1597ea9267b9dc5b29998c9d47e3095",
+    "url": "/climate-change-storyboard/static/media/country-flags.a1597ea9.csv"
+  },
+  {
+    "revision": "72529182c48b364f1c7af7aec418bf35",
+    "url": "/climate-change-storyboard/static/media/county-temp-1979-2011.72529182.csv"
+  },
+  {
+    "revision": "13a1838a5e82d94365fc83980154f9c7",
+    "url": "/climate-change-storyboard/static/media/co2-ppm-0-2014.13a1838a.csv"
+  },
+  {
+    "revision": "8cbb06c4f4c897e5f34e4fa09bccf382",
+    "url": "/climate-change-storyboard/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/climate-change-storyboard/precache-manifest.633ce79f51a2ca6584a7511447436eec.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/climate-change-storyboard/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
