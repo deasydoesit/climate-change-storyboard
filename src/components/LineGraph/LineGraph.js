623a762ed68c52144508f53050f7faf6c23d79d8
deasydoesit <darwin.rinderer@gmail.com>
2018-12-24 21:56:10 -0500

    Updating hero styling

--- src/components/LineGraph/LineGraph.js
+++ src/components/LineGraph/LineGraph.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 import { XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalGridLines, LineSeries, Hint } from 'react-vis';
+import { Button } from 'reactstrap';
 
 // Internal imports
 import co2Data500 from '../../data/LineGraph/co2-ppm-0-500.csv';
@@ -21,6 +22,7 @@ class LineGraph extends Component {
     co2DataTotal: [],
     data: [],
     segmentIndex: 0,
+    lineGraphHeader: "For centuries, and even thousands of years, atmospheric carbon dioxide emissions remained steady ...",
     value: false,
   };
 
@@ -52,35 +54,53 @@ class LineGraph extends Component {
   handleClick = () => {
     const { segmentIndex } = this.state;
     const co2Segments = ['co2Data1000', 'co2Data1500', 'co2Data2000', 'co2DataTotal'];
+    const textSegments = [
+      'For centuries, and even thousands of years, atmospheric carbon dioxide emissions remained steady. '
+
+    ]
+
     const segment = co2Segments[segmentIndex];
-    console.log(segmentIndex);
+
     if (segmentIndex <= 2) {
       this.setState({ 
         data: this.state.data.concat(this.state[segment]),
+        lineGraphHeader: this.state.lineGraphHeader.concat(textSegments[segmentIndex]),
         segmentIndex: this.state.segmentIndex + 1,
       });
     }
   }
 
   render() {
-    const { segmentIndex } = this.state;
+    const { segmentIndex, lineGraphHeader } = this.state;
     return (
       <div className='graph-container'>
-        <button onClick={() => this.handleClick()}>
-          click me
-        </button>
+        <h1 className='line-graph-header'>{lineGraphHeader}</h1>
+        <div className='line-graph-btn-container'>
+          <Button outline color="danger" className='line-graph-btn' onClick={() => this.handleClick()} >
+            <span className='icon-holder'>
+              <img src="https://image.flaticon.com/icons/svg/56/56380.svg" alt="One finger tap gesture of outlined hand symbol" class="replaced-svg" /> 
+            </span>
+            click to continue
+          </Button>
+        </div>
         <XYPlot 
           animate={true}
           className='line-graph-plot'
           onMouseLeave={() => this.setState({value: false})}
           width={700} 
-          height={500}
-          yDomain={ segmentIndex === 3 ? null : [250, 300]}
+          height={400}
+          yDomain={ segmentIndex === 3 ? null : [250, 300] }
         >
           <HorizontalGridLines />
           <VerticalGridLines />
-          <XAxis />
-          <YAxis />
+          <XAxis 
+            position="end"
+            title="Year"
+          />
+          <YAxis 
+            position="end"
+            title="Carbon dioxide level (ppm)"
+          />
           <LineSeries
             className='line-graph-path'
             curve={'curveMonotoneX'}
